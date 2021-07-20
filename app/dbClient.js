52045d1c4243d31b93cbb2db8d6fa03ee7d5022f
Delahaye <delahayeguilhem@gmail.com>
2021-07-20 17:29:28 +0200

    ready to deploy

--- app/dbClient.js
+++ app/dbClient.js
@@ -2,7 +2,7 @@
 const {Client} = require('pg');
 
 // Create the client
-let client = new Client('postgres://washathome:washathome@localhost:5432/washathome');
+let client = new Client(process.env.PG_URL);
 if (process.env.NODE_ENV === "production") {
     client = new Client({
         connectionString: process.env.DATABASE_URL,
--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-// Imports
+// Imports and dotenv config
+require('dotenv').config();
 const express = require('express')
 const router = require('./app/router')
-
-// Dotenv import and declaration
-require('dotenv').config();
-const port = process.env.PORT || 8888;
 
 // Express app declaration
 const app = express();
 
-// Router
+// Add prefix '/v1' to every routes
 app.use('/v1',router);
 
 // Port listening
+const port = process.env.PORT || 8888;
+
 app.listen(port, () => {
     console.log(`Server running : http://localhost:${port}`);
 });
