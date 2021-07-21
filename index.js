57212e26b9bf7ecbf48333b6ceb001f3f57ef539
Delahaye <delahayeguilhem@gmail.com>
2021-07-21 10:23:04 +0200

    deployment fix cors added

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // Imports and dotenv config
 require('dotenv').config();
 const express = require('express')
+const cors = require ("cors");
 const router = require('./app/router')
 
 // Express app declaration
 const app = express();
+
+// Cross-origin resource sharing   
+app.use(cors());
 
 // Add prefix '/v1' to every routes
 app.use('/v1',router);
