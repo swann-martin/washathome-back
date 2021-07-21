6b6edd5929aed72fa6d63302da453e60c5a13c54
Magami-Sinbad21 <mag-olivier@outlook.com>
2021-07-21 15:14:12 +0200

    creation of domains in sqitch

---
+++ migrations/revert/domain.sql
@@ -0,0 +1,19 @@
+-- Revert washathome:domain from pg
+
+BEGIN;
+
+ALTER TABLE machine
+    ALTER COLUMN zip_code TYPE INT
+    USING zip_code::INT;
+
+DROP DOMAIN zipcode;
+
+
+
+ALTER TABLE "user" 
+    ALTER COLUMN phone TYPE INT
+    USING phone :: INT;
+
+DROP DOMAIN mobilphone;
+
+COMMIT;
