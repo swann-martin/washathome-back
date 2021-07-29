5cef32a1236be2514beebdfb153e7908282e3d6f
Delahaye <delahayeguilhem@gmail.com>
2021-07-29 16:17:28 +0200

    request added in the login

--- app/models/User.js
+++ app/models/User.js
@@ -45,8 +45,31 @@ class User {
     }
 
     // Find by mail and send machines method
-    static async findByMailJoin (mail) {
-        const {rows} = await db.query('SELECT * FROM "user" FULL OUTER JOIN machine ON "user".id = machine.user_id WHERE "user".mail = $1 ;' , [mail]);
+    static async findByIdJoin (mail) {
+        const {rows} = await db.query(`SELECT
+                                        json_build_object('user_id', "user".id, 'user_pseudo', "user".pseudo,'user_lastname',"user".lastname,'user_firstname', "user".firstname,'user_phone',"user".phone,'user_mail',"user".mail,'user_avatar',"user".avatar,'user_password',"user".password)"user",
+                                        json_build_object(
+                                            'machine_id', machine.id,
+                                            'machine_name', machine.name,
+                                            'machine_address',machine.address,
+                                            'machine_zip_code',machine.zip_code,
+                                            'machine_city',machine.city,
+                                            'machine_latitude',machine.latitude,
+                                            'machine_longitude',machine.longitude,
+                                            'dispos', ARRAY_AGG (json_build_object(
+                                                'availibility_id',availibility.id,
+                                                'availibility_open_hour', To_char(availibility.open_hour,'HH24:MI - DD/MM/YYYY'),
+                                                'availibility_end_hour', To_char(availibility.end_hour,'HH24:MI - DD/MM/YYYY'),
+                                                'availibility_booked',availibility.booked ,
+                                                'machine_id',machine.id)
+                                        )) machine
+                                    FROM 
+                                        "user"
+                                    FULL OUTER JOIN machine ON "user".id=machine.user_id 
+                                    FULL OUTER JOIN availibility ON machine.id = availibility.machine_id
+                                    WHERE "user".id= $1
+                                    GROUP BY ("user".id,machine.id);
+    ` , [mail]);
         console.log(rows);
         return rows.map(row => new User(row));
     }
