3553c286015b7456828ed6b8752829d9ad27ee02
Delahaye <delahayeguilhem@gmail.com>
2021-08-02 14:28:16 +0200

    token fix

--- app/middleware/authMiddleware.js
+++ app/middleware/authMiddleware.js
@@ -4,17 +4,22 @@ const jwt = require('jsonwebtoken');
 // Middleware object exported
 module.exports = function (req,res,next){
   try{
+    console.log(req.headers);
     //Check authorization existence
     if (!req.headers.authorization) {throw new Error("Error. No authorization property in the request header" )}
 
+    // Keep the first part of the token in the header
+    const token = req.headers.authorization.split(' ')[0]
+
+    // Check token existence
+    if (token == null) {throw new Error("Error. There is no token in the authorization header" )}
+
     // Verify concordance with passphrase
-    jwt.verify(req.headers.authorization,'TOKEN_SECRET', (err, user) => {
+    jwt.verify(token,'TOKEN_SECRET', (err, user) => {
       console.log(err)
       if (err) {throw new Error( "Error. Wrong token" )}
       req.user = user
     })
-
-    // Pass the request object to the next function
     next()
   }
   catch(error){
